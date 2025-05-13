25b122c2fc76937a5ca4c2af958f8846a0ffa283
srevuelta <srevuelta@hyg.com.co>
2025-05-13 12:24:03 -0500

    add: add server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-import os, sys
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Management.settings")
 
 application = get_wsgi_application()
