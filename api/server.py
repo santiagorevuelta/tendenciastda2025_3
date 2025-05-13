52a3206c80747e82fccad0292d72a4425e707a7a
srevuelta <srevuelta@hyg.com.co>
2025-05-13 12:53:00 -0500

    update: favicon.ico

--- api/server.py
+++ api/server.py
@@ -2,7 +2,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Management.settings")
 
 application = get_wsgi_application()
