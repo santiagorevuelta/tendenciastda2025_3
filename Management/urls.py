61719c64b4533aaedebe28e60863640f7891aa34
Santi.rev <38337875+santiagorevuelta@users.noreply.github.com>
2025-05-31 14:42:07 -0500

    Update urls.py

--- Management/urls.py
+++ Management/urls.py
@@ -41,9 +41,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('productos.urls')),
     path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('productos.urls')),
 ]
