573d99d06cad9e4ef84db926a0c8f90d7a827afe
Santi.rev <38337875+santiagorevuelta@users.noreply.github.com>
2025-05-31 12:44:29 -0500

    Update urls.py

--- Management/urls.py
+++ Management/urls.py
@@ -42,7 +42,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('productos.urls')),
-    path('metrics/', include('django_prometheus.urls')),
     path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
